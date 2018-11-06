0d73039e8383700bad85a646dffb73d95615f854
FrankFan <fanyong@gmail.com>
2018-11-06 23:53:37 +0800

    add filter

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -4,7 +4,7 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    data: Object,
   },
 
   /**
@@ -12,6 +12,10 @@ Component({
    */
   data: {
 
+  },
+
+  ready() {
+    console.log(`test = ${JSON.stringify(this.data) }`);
   },
 
   /**
--- components/Tag/Tag.js
+++ components/Tag/Tag.js
@@ -4,14 +4,43 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    tagType: {
+      type: String,
+      value: 'pop',
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    text: '',
+  },
+  
+  ready() {
+    const type = this.data.tagType;
+    let text = '';
+    let clazz = '';
+    if(type === 'top') {
+      text = '置顶';
+      clazz = 'green';
+    } else if(type === 'good') {
+      text = '精华';
+      clazz = 'green';
+    } else if (type === 'share') {
+      text = '分享';
+      clazz = 'gray';
+    } else if (type === 'ask') {
+      text = '问答';
+      clazz = 'gray';
+    } else if (type === 'job') {
+      text = '招聘';
+      clazz = 'gray';
+    }
+    this.setData({
+      clazz,
+      text,
+    });
   },
 
   /**
