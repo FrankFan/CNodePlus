cf1762957992daae5c799999c2435eb940eb5807
Khan Fan <khan.fan@oriente.com>
2018-11-09 12:48:57 +0800

    add new feature: support render comments

---
+++ components/Comment/Comment.js
@@ -0,0 +1,29 @@
+// components/Comment/Comment.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    reply: {
+      type: Object,
+      value: [],
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  ready () {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -6,11 +6,12 @@ const app = getApp();
 Page({
   data: {
     article: {},
+    reply: {},
   },
   tap() {
     console.log('tap')
   },
-  onShareAppMessage: function (res) {
+  onShareAppMessage: function(res) {
     console.log(`id = ${this.data.id}`);
     return {
       title: this.data.title,
@@ -23,13 +24,15 @@ Page({
     });
     var id = options.id;
     this.getDetail(id);
-    wx.setNavigationBarTitle({ title: 'Detail' });
+    wx.setNavigationBarTitle({
+      title: 'Detail'
+    });
   },
   onPullDownRefresh() {
     console.log('onPullDownRefresh');
     setTimeout(function() {
       wx.stopPullDownRefresh();
-    } ,1500);
+    }, 1500);
   },
   onReachBottom() {
     console.log('onReachBottom');
@@ -45,22 +48,35 @@ Page({
           author,
           title,
           create_at,
+          replies,
         } = res.data.data;
         let data = app.towxml.toJson(article, 'markdown');
+        let repliesFormatData = [];
+        if (replies.length > 0) {
+          repliesFormatData = replies.map((item, index) => {
+            return {
+              avatarUrl: item.author.avatar_url,
+              loginname: item.author.loginname,
+              index: index + 1,
+              replyDate: util.formatTime(new Date(create_at)),
+              content: item.content,
+            }
+          });
+        }
         this.setData({
           id,
           article: data,
           author,
           title,
-          create_at: util.formatTime(new Date(create_at))
+          create_at: util.formatTime(new Date(create_at)),
+          replies: repliesFormatData,
         });
         wx.hideLoading();
       },
       fail: (res) => {
         console.log(res);
       },
-      complete: res => {
-      }
+      complete: res => {}
     })
   }
 })
